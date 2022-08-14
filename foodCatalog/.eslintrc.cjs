503ff2635e684b920e2be8ac5d176084ecbdf369
Au Yong Ting Ting <tingting.a@affinidi.com>
2022-08-14 18:09:59 +0800

    react boiler plate

---
+++ foodCatalog/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    "jsx-quotes": ["error", "prefer-single"],
+    semi: ["error", "never"],
+    "no-unused-vars": [2, { args: "none" }],
+    "comma-dangle": ["error"],
+  },
+};
